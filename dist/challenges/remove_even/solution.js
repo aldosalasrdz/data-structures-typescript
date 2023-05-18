34c86c74ed24f921cc960217f77b3128d04339af
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-05-18 17:46:44 -0600

    refactor: remove js files

--- dist/challenges/remove_even/solution.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.removeEven = void 0;
-function removeEven(arr) {
-    return arr.filter((num) => num % 2 !== 0);
-}
-exports.removeEven = removeEven;
