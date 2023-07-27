8d8b875f6bdd5f7abff4e10b5c3d57970b1ef650
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-07-26 23:47:13 -0600

    add: queue helper functions

--- src/queues/Queue.ts
+++ src/queues/Queue.ts
@@ -2,4 +2,20 @@ import { DoublyLinkedList } from "./DoublyLinkedList";
 
 export class Queue {
   public items = new DoublyLinkedList();
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  getFront() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      return this.items.head;
+    }
+  }
+
+  size() {
+    return this.items.length;
+  }
 }
