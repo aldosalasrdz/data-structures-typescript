a9e69df82a14d926bba1c3c5dc6fb196ced74975
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-07-28 01:05:55 -0600

    add: generate binary numbers from 1 to n challenge

---
+++ src/queues/challenges/find-bin/solution.ts
@@ -0,0 +1,14 @@
+import { Queue } from "../../Queue";
+
+function findBin(num: number) {
+  const queue = new Queue<number>();
+
+  for (let i = 1; i <= num; i++) {
+    const binaryNumber = Number((i >>> 0).toString(2));
+    queue.enqueue(binaryNumber);
+  }
+
+  return queue.items.toString();
+}
+
+console.log(findBin(10));
